efc2aa63f2b1b37a85c5303c13540760951411fc
Devank <singhdevank177@gmail.com>
2025-03-22 20:01:29 +0530

    added logo

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Profile = () => {
   return (
-    <div>Profile</div>
+   <>
+   <main className='flex-1 justify-center items-center'>
+    <div className='h-[80vh] font-mono flex justify-center items-center mx-auto text-center'>This feature is coming soon.....</div>
+   </main>
+   </>
   )
 }
 
